f130f1722d79b422f69aa0b2f3003a49bfb57cc6
BM835 <devdev@bm835.space>
2019-07-26 22:19:49 +1000

    retry added

--- main.sh
+++ main.sh
@@ -1,28 +1,37 @@
-echo "Input first number: "
-read n1
+retry="y"
 
-echo "Input operator: "
-read op
-
-echo "Input second number: "
-read n2
-
-ans=0
-if [ $op = "+" ]
-then
-	ans=$((n1+n2))
-fi
-if  [ $op = "-" ]
-then
-	ans=$((n1-n2))
-fi
-if [ "$op" = "*" ]
-then
-	ans=$((n1*n2))
-fi
-if [ $op = "/" ]
-then
-	ans=$((n1/n2))
-fi
-
-echo "$n1 $op $n2 = $ans"
+while [ $retry = "y" ]
+do
+	echo "Input first number: "
+	read n1
+	
+	echo "Input operator: "
+	read op
+	
+	echo "Input second number: "
+	read n2
+	
+	ans=0
+	if [ $op = "+" ]
+	then
+		ans=$((n1+n2))
+	fi
+	if  [ $op = "-" ]
+	then
+		ans=$((n1-n2))
+	fi
+	if [ "$op" = "*" ]
+	then
+		ans=$((n1*n2))
+	fi
+	if [ $op = "/" ]
+	then
+		ans=$((n1/n2))
+	fi
+	
+	echo "$n1 $op $n2 = $ans"
+	
+	
+	echo "Retry? (y/n)"
+	read retry
+done
