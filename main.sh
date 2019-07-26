f4c0aa9f74f0a2066a05673761475ccdd64dfdfc
BM835 <devdev@bm835.space>
2019-07-26 22:13:50 +1000

    Init

---
+++ main.sh
@@ -0,0 +1,28 @@
+echo "Input first number: "
+read n1
+
+echo "Input operator: "
+read op
+
+echo "Input second number: "
+read n2
+
+ans=0
+if [ $op = "+" ]
+then
+	ans=$((n1+n2))
+fi
+if  [ $op = "-" ]
+then
+	ans=$((n1-n2))
+fi
+if [ "$op" = "*" ]
+then
+	ans=$((n1*n2))
+fi
+if [ $op = "/" ]
+then
+	ans=$((n1/n2))
+fi
+
+echo "$n1 $op $n2 = $ans"
